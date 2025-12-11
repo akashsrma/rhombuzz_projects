6f818f2cfd1547d4e25e61a5e44b3dd46a756dcc
akash <akashsrma3695@gmail.com>
2025-12-11 11:57:16 +0530

    feat: landing content and css added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 export default function Landing({ onStart }) {
-  return <div>First Page</div>;
+  return (
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1 className="landing-title">
+          Your modern <br />
+          <span className="highlight">analytics portal</span>
+        </h1>
+
+        <p className="landing-subtitle">
+          A beautiful, secure dashboard with real-time metrics, built with React
+          and Sign in with Custom Provider.
+        </p>
+
+        <div className="landing-buttons">
+          <button className="btn started-btn" onClick={onStart}>
+            Get Started <HiArrowNarrowRight className="arrow-btn" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
